3d173dd70077326d573c4c04f57b9eea12769933
Sergio Ley Languren <sergioleylanguren@Sergios-MacBook-Pro.local>
2022-09-15 19:22:52 -0700

    added intensify & reinforce functions to Newspeak.py + created test.py

--- 02-Newspeak/Newspeak.py
+++ 02-Newspeak/Newspeak.py
@@ -1,5 +1,5 @@
 # File: Newspeak.py
-# Name: your name
+# Name: Sergio Ley Languren
 
 """
 This module defines several functions that implement several of the
@@ -7,5 +7,17 @@ standard prefixes used in the Newspeak language described in George
 Orwell's novel 1984.
 """
 
-# Your job is to write the functions negate, intensify, and reinforce
-# along with a test program to make sure that all the functions work.
+def negate(word: str) -> str:
+    """appplies "un" to given words"""
+    result = "un" + word
+    return result
+
+def intensify(word: str) -> str:
+    """applies "plus" to given words"""
+    result = "plus" + word
+    return result
+
+def reinforce(word: str) -> str:
+    """applies "double" to given words"""
+    result = "double" + word
+    return result
---
+++ 02-Newspeak/test.py
@@ -0,0 +1,43 @@
+# File: testp.py
+# Author: Sergio Ley Languren
+from Newspeak import negate, intensify, reinforce
+from time import sleep
+
+
+# Assertion Error class
+class WordIncorrect(AssertionError):
+    def __str__(self):
+        return f'''Word Incorrect warning: {self.args[0]}'''
+
+
+# negate function test 
+word1 = "cold"
+
+print(f"the word is {word1}. The expected result is: un{word1}")
+sleep(1)
+result = negate(word1)
+print("Result:\n")
+sleep(1)
+print(result)
+assert result == "uncold", WordIncorrect(f"{result} did not meet the expected result of: uncold")
+
+# intensify function test
+print(f"the word is {word1}. The expected result is: plus{word1}")
+sleep(1)
+result = intensify(word1)
+print("Result:\n")
+sleep(1)
+print(result)
+assert result == "pluscold", WordIncorrect(f"{result} did not meet the expected result of: pluscold")
+
+# reinforce test
+word2 = "good"
+print("Test reinforce.1:\n")
+sleep(1)
+print(f"the word is {word1}. The expected result is: doubleplus{word1}")
+sleep(1)
+result = reinforce(intensify(word1))
+print("Result:\n")
+sleep(1)
+print(result)
+assert result == "doublepluscold", WordIncorrect(f"{result} did not meet the expected result of: doublepluscold")
